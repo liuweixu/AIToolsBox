fee670c54f72841fa0822ff7b5cebd2e3477616f
liuweixu <liuweixu2018@gmail.com>
2025-10-27 20:51:06 +0800

    大模型接入 完成

---
+++ src/test/java/org/example/chatreact/ChatReactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
